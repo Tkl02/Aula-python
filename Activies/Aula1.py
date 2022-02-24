9757c777d97b4c426c78aa5797cb2eba6212b8db
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-02-24 09:58:03 -0300

    termino da revisao da ativ

---
+++ Activies/Aula1.py
@@ -0,0 +1,8 @@
+# Aula 1: calcule a soma entre dois numeros.
+
+A = int(input('digite o valor de A:'))
+B = int(input('digite o valor de B:'))
+
+C = A + B
+
+print('a soma dos numeros e igual a:{}'.format(C))
