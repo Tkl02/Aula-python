14c16b376bf684d7c8ac5886a8309455ff060c12
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-05-06 10:08:02 -0300

    .

--- Activies/test.py
+++ Activies/test.py
@@ -1,24 +1,10 @@
 # Uso para testar modificaçoes nos coodigos separadamente
 
-from time import sleep
+import math
 
+r = float(input("numero: "))
+n = 3.14159
 
-print('-=-'*10)
-print('    analisar o trinagulo')
-print('-=-'*10)
-sleep(1)
+a = n * (r**2)
 
-r1 = float(input('primeira reta: '))
-r2 = float(input('segunda reta: '))
-r3 = float(input('terceira reta: '))
-
-print('-=-'*10)
-print('         ANALISANDO...')
-print('-=-'*10)
-
-sleep(1.8)
-
-if r1 < r2 + r3 and r2 < r3 + r1 and r3 < r1 + r2:
-    print('e possivel fazer um triangulo')
-else:
-    print('e impossivel fazer um triangulo')
+print("R = {:.4f}".format(a))
