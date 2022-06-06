4aaa3fcb8c49853cc34597bf785b9ffccbcdec5e
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-06-06 11:10:10 -0300

    .

---
+++ faculdade/aula5.py
@@ -0,0 +1,5 @@
+from barcode import EAN13
+from barcode.writer import ImageWriter
+
+with open (r'C:\Users\second\Pictures\Camera Roll\test.png', 'wb') as f:
+    EAN13(str(123456789854), writer = ImageWriter()).write(f)
