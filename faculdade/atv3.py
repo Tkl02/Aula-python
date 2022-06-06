cf6fe8e7288f962036c6d91cfdb2734b1569e6b1
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-06-06 10:12:50 -0300

    .

--- faculdade/atv3.py
+++ faculdade/atv3.py
@@ -1,14 +1,9 @@
 from gtts import gTTS             #bibliotecas
 from playsound import playsound
-import pyttsx3
-
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
 
 text = str(input("qual a frase? "))  #variavel para selecionar texto
-idioma = str(input("qual idioma"))   #variavel para selecionar idioma
-
+# idioma = str(input("qual idioma"))   #variavel para selecionar idioma
+idioma = 'pt-br'
 audio = 'meusom.mp3'   #atribuindo nome para o audio
 
 convers = gTTS (       #convertendo o codigo para audio
