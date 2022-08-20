0392cafa6ee9826c05f938c977ad27c5752f2141
Tkl02 <leonardofaustino0@gmail.com>
2022-08-20 18:15:25 -0300

    .

--- faculdade/test2.py
+++ faculdade/test2.py
@@ -1,5 +1,9 @@
-import pyautogui
+import time
 
-ts = pyautogui.position()
-
-print(ts)
+numero_1 = 0
+numero_2 = 1
+while (numero_1 < numero_2):
+       print(numero_1)
+       numero_1   = numero_1 + 1
+       numero_2   = numero_2 + 1
+       time.sleep(0.4)
