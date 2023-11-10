50c8cbf920ad483864e739ef403729be66d01d9e
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2023-11-10 16:03:29 -0300

    .

--- mundo_python_2/for/atv2.py
+++ mundo_python_2/for/atv2.py
@@ -2,4 +2,4 @@
 
 for i in range(1,51):
     if i % 2==0:
-        print(i)
+        print(i, end=", ")
--- pj_aprendizado/conct.py
+++ pj_aprendizado/conct.py
@@ -1,6 +1,22 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = "0.0.0.0"
+port = 8888
 
 try:
-    server.bind()
+    server.bind((ip, port))
+    server.listen(5)
+    print(f"escutando:{ip}:{port}")
+
+    (client_socket, address)=server.accept()
+
+    while True:
+        data = client_socket.recv(1500)
+        
+        if data == "exit":
+            server.close()
+    
+except Exception as error:
+    print (error)
+    server.close()
